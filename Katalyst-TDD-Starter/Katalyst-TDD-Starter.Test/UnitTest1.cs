30b37b89f8e796aa3c06a61d093a4dad85ed8c55
Daniel Russom <daniel.russom00@gmail.com>
2022-05-22 16:08:58 +0100

    init test project

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
