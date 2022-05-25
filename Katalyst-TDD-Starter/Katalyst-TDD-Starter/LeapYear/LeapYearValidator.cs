48670bd7ec7ec2dd54e41da2eec4108877712894
Daniel Russom <daniel.russom00@gmail.com>
2022-05-25 17:19:23 +0100

    Improve parameter names for LeapYearValidator

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter/LeapYear/LeapYearValidator.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/LeapYear/LeapYearValidator.cs
@@ -17,9 +17,9 @@
             return IsDivisibleBy(input, 4);
         }
 
-        private static bool IsDivisibleBy(int input, int number)
+        private static bool IsDivisibleBy(int value, int divideBy)
         {
-            return input % number == 0;
+            return value % divideBy == 0;
         }
     }
 }
