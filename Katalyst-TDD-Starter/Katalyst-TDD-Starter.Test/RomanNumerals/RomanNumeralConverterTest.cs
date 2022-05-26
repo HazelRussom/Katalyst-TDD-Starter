24dbe0458b8dfcca18b9ebbe73dc7bf54632781c
Daniel Russom <daniel.russom00@gmail.com>
2022-05-26 22:22:10 +0100

    Roman numeral conversion 4 through 8

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/RomanNumerals/RomanNumeralConverterTest.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/RomanNumerals/RomanNumeralConverterTest.cs
@@ -17,6 +17,11 @@ namespace Katalyst_TDD_Starter.Test.RomanNumerals
         [DataRow(1, "I")]
         [DataRow(2, "II")]
         [DataRow(3, "III")]
+        [DataRow(4, "IV")]
+        [DataRow(5, "V")]
+        [DataRow(6, "VI")]
+        [DataRow(7, "VII")]
+        [DataRow(8, "VIII")]
         public void Arabic_number_should_become_Roman_numeral_equivalent(int input, string expected)
         {
             var actual = ToTest.Convert(input);
--- Katalyst-TDD-Starter/Katalyst-TDD-Starter/RomanNumerals/RomanNumeralConverter.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/RomanNumerals/RomanNumeralConverter.cs
@@ -4,7 +4,20 @@
     {
         public string Convert(int input)
         {
+            if (input == 4)
+            {
+                return "IV";
+            }
+
             var result = string.Empty;
+
+            var five_Count = input / 5;
+
+            for(int i = 0; i < five_Count; i++)
+            {
+                result += "V";
+                input -= 5;
+            }
 
             for(int i = 0; i < input; i++)
             {
