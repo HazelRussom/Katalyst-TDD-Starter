8b4919c292d0d5df8e2b415cbc88a60580409653
Daniel Russom <daniel.russom00@gmail.com>
2022-05-31 23:33:07 +0100

    TextProcessor first test

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/TextProcessing/TextProcessorTests.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/TextProcessing/TextProcessorTests.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Katalyst_TDD_Starter.TextProcessing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Katalyst_TDD_Starter.Test.TextProcessing
 {
     [TestClass]
     public class TextProcessorTests
     {
+        public TextProcessorTests()
+        {
+            ToTest = new TextProcessor();
+        }
+
+        public TextProcessor ToTest { get; private set; }
 
         [TestMethod]
         public void The_output_should_contain_the_total_word_count()
         {
             var input = string.Empty;
 
+            var expected = "This text has 0 words in total";
 
+            var actual = ToTest.Process(input); 
 
+            Assert.IsTrue(actual.Contains(expected));
         }
     }
 }
---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/TextProcessing/TextProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace Katalyst_TDD_Starter.TextProcessing
+{
+    public class TextProcessor
+    {
+        public string Process(string input)
+        {
+            return "This text has 0 words in total";
+        }
+    }
+}
