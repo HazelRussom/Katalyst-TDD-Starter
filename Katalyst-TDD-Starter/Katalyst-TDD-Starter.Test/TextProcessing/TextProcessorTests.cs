af701c0a9c37bdffab9bcdd2af17cccba9a2cdcd
Daniel Russom <daniel.russom00@gmail.com>
2022-05-31 23:29:40 +0100

    Init TextProcessor tests

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/TextProcessing/TextProcessorTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.TextProcessing
+{
+    [TestClass]
+    public class TextProcessorTests
+    {
+
+        [TestMethod]
+        public void The_output_should_contain_the_total_word_count()
+        {
+            var input = string.Empty;
+
+
+
+        }
+    }
+}
