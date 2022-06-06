4b8246b6647023d0341af090c45c83d144a7e63b
Daniel Russom <daniel.russom00@gmail.com>
2022-06-06 20:14:35 +0100

    Initial Bag Add test

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/StorageBagTests.cs
@@ -0,0 +1,20 @@
+﻿using Katalyst_TDD_Starter.Bags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.Bags
+{
+    [TestClass]
+    public class StorageBagTests
+    {
+        [TestMethod]
+        public void Leather_should_be_added_to_bag()
+        {
+            var bag = new StorageBag();
+            var input = "Leather";
+
+            bag.Add(input);
+
+            Assert.IsTrue(bag.Items.Contains(input));
+        }        
+    }
+}
---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bags/StorageBag.cs
@@ -0,0 +1,16 @@
+﻿namespace Katalyst_TDD_Starter.Bags
+{
+    public class StorageBag
+    {
+        public StorageBag()
+        {
+        }
+
+        public List<string> Items { get; internal set; } = new();
+
+        public void Add(string input)
+        {
+            Items.Add(input);
+        }
+    }
+}
