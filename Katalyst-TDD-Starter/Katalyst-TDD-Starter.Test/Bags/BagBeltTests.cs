946703ca2859b0db673a695300ffa40ea2f5b9aa
Daniel Russom <daniel.russom00@gmail.com>
2022-06-07 17:55:40 +0100

    init bag belt

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/BagBeltTests.cs
@@ -0,0 +1,30 @@
+﻿using Katalyst_TDD_Starter.Bags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.Bags
+{
+    [TestClass]
+    public class BagBeltTests
+    {
+        public BagBelt ToTest { get; private set; }
+
+        public BagBeltTests()
+        {
+            ToTest = new BagBelt();
+        }
+
+        [TestMethod]
+        [DataRow (1)]
+        [DataRow (2)]
+        [DataRow (4)]
+        public void Bag_belt_can_hold_bags(int numberOfBags)
+        {
+            for (int i = 0; i < numberOfBags; i++)
+            {
+                ToTest.AddBag(new StorageBag());
+            }
+
+            Assert.IsTrue(ToTest.Bags.Count == numberOfBags);
+        }
+    }
+}
---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bags/BagBelt.cs
@@ -0,0 +1,12 @@
+﻿namespace Katalyst_TDD_Starter.Bags
+{
+    public class BagBelt
+    {
+        public List<StorageBag> Bags { get; set; } = new();
+
+        public void AddBag(StorageBag input)
+        {
+            Bags.Add(input);
+        }
+    }
+}
