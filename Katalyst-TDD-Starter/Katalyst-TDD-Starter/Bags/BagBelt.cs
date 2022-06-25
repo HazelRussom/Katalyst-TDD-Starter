aad20ad9c1993349ae13ba1931dc8b94d25c38bb
Daniel Russom <daniel.russom00@gmail.com>
2022-06-25 19:36:21 +0100

    Sorting herbs - rule of 3 refactor

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bags/BagBelt.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bags/BagBelt.cs
@@ -23,59 +23,20 @@ namespace Katalyst_TDD_Starter.Bags
 
         public void OrganiseBags()
         {
-            // Get all items that are in the wrong bag
-            // Remove items from their bags
-            // Store locally
-            // Put each item into the correct bag
-            //var clothBags = Bags.Where(x => x.Category == ItemCategory.Cloth);
+            var itemCategories = Enum.GetValues(typeof(ItemCategory));
 
-            //if (clothBags.Count() > 0)
-            //{
-            //    var unsortedCloth = new List<Item>();
-            //    foreach (StorageBag bag in Bags)
-            //    {
-            //        var currentUnsortedItems = bag.Items.Where(x => x.Category != bag.Category && x.Category == ItemCategory.Cloth).ToList();
-            //        bag.Remove(currentUnsortedItems);
-            //        unsortedCloth.AddRange(currentUnsortedItems);
-            //    }
-
-            //}
-
-            //if(Bags.Any(x => x.Category == ItemCategory.Cloth && x.HasSpace()))
-            //{
-
-            //}
-
-            var unsortedCloth = Bags[0].Items.Where(x => x.Category == ItemCategory.Cloth).ToList();
-            var clothBag = Bags.Where(x => x.Category == ItemCategory.Cloth).First();
-
-            foreach (var item in unsortedCloth)
+            foreach(ItemCategory category in itemCategories)
             {
-                clothBag.Items.Add(item);
-                Bags[0].Remove(item);
-            }
+                var unsortedItems = Bags[0].Items.Where(x => x.Category == category).ToList();
+                var categoryBag = Bags.Where(x => x.Category == category).FirstOrDefault();
 
-            var unsortedMetal = Bags[0].Items.Where(x => x.Category == ItemCategory.Metal).ToList();
-            var metalBag = Bags.Where(x => x.Category == ItemCategory.Metal).FirstOrDefault();
-
-            if (metalBag != null)
-            {
-                foreach (var item in unsortedMetal)
+                if (categoryBag != null)
                 {
-                    metalBag.Items.Add(item);
-                    Bags[0].Remove(item);
-                }
-            }
-
-            var unsortedHerbs = Bags[0].Items.Where(x => x.Category == ItemCategory.Herb).ToList();
-            var herbBag = Bags.Where(x => x.Category == ItemCategory.Herb).FirstOrDefault();
-
-            if (herbBag != null)
-            {
-                foreach (var item in unsortedHerbs)
-                {
-                    herbBag.Items.Add(item);
-                    Bags[0].Remove(item);
+                    foreach (var item in unsortedItems)
+                    {
+                        categoryBag.Items.Add(item);
+                        Bags[0].Remove(item);
+                    }
                 }
             }
         }
