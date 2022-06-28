2c1de2e13695f9d28ebe5f46e05f6b1458e663b9
Daniel Russom <daniel.russom00@gmail.com>
2022-06-28 13:32:32 +0100

    Init Password Validation

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/PasswordValidation/PasswordValidatorTests.cs
@@ -0,0 +1,16 @@
+﻿using Katalyst_TDD_Starter.PasswordValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.PasswordValidation
+{
+    [TestClass]
+    public class PasswordValidatorTests
+    {
+        [TestMethod]
+        public void Empty_string_should_return_false()
+        {
+            var validator = new PasswordValidator();
+            Assert.IsFalse(validator.Validate(string.Empty));
+        }
+    }
+}
---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/PasswordValidation/PasswordValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Katalyst_TDD_Starter.PasswordValidation
+{
+    public class PasswordValidator
+    {
+        public PasswordValidator()
+        {
+        }
+
+        public bool Validate(string empty)
+        {
+            return false;
+        }
+    }
+}
