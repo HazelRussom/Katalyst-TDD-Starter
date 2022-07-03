c41439a406ccacc51333956b17cc16f617f8c8fd
Daniel Russom <daniel.russom00@gmail.com>
2022-07-03 21:06:27 +0100

    PasVal : Input Length in Builder

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/PasswordValidation/PasswordValidatorBuilderTests.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/PasswordValidation/PasswordValidatorBuilderTests.cs
@@ -6,14 +6,28 @@ namespace Katalyst_TDD_Starter.Test.PasswordValidation
     [TestClass]
     public class PasswordValidatorBuilderTests
     {
+        public PasswordValidatorBuilder UnderTest { get; set; }
+
+        public PasswordValidatorBuilderTests()
+        {
+            UnderTest = new PasswordValidatorBuilder();
+        }
+
         [TestMethod]
         public void Building_the_validator_should_return_a_validator_object()
         {
-            var UnderTest = new PasswordValidatorBuilder();
-
             var result = UnderTest.Build();
 
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Validator_with_input_length_should_have_value_set()
+        {
+            var inputLength = 5;
+            var result = UnderTest.WithInputLength(inputLength).Build();
+
+            Assert.AreEqual(inputLength, result.InputLength);
         }
     }
 }
--- Katalyst-TDD-Starter/Katalyst-TDD-Starter/PasswordValidation/PasswordValidatorBuilder.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/PasswordValidation/PasswordValidatorBuilder.cs
@@ -2,13 +2,21 @@
 {
     public class PasswordValidatorBuilder
     {
+        private int _inputLength = 9;
+
         public PasswordValidatorBuilder()
         {
         }
 
         public PasswordValidator Build()
         {
-            return new PasswordValidator();
+            return new PasswordValidator().SetInputLength(_inputLength);
+        }
+
+        public PasswordValidatorBuilder WithInputLength(int inputLength)
+        {
+            _inputLength = inputLength;
+            return this;
         }
     }
 }
