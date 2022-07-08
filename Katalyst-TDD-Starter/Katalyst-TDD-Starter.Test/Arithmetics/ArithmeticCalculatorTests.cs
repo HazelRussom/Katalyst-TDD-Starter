a35aaeadde5c0e9070acc07f800ea0b2b17b0ad8
Daniel Russom <daniel.russom00@gmail.com>
2022-07-08 23:09:05 +0100

    Init Arithmetic Kata

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Arithmetics/ArithmeticCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Katalyst_TDD_Starter.Arithmetics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.Arithmetics
+{
+    [TestClass]
+    public class ArithmeticCalculatorTests
+    {
+        [TestMethod]
+        public void Parentheses_should_return_0()
+        {
+            var UnderTest = new ArithmeticCalculator();
+
+            var result = UnderTest.Calculate("()");
+
+            Assert.AreEqual(result, "0");
+        }
+
+    }
+}
---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/Arithmetics/ArithmeticCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Katalyst_TDD_Starter.Arithmetics
+{
+    public class ArithmeticCalculator
+    {
+        public ArithmeticCalculator()
+        {
+
+        }
+
+        public string Calculate(string input)
+        {
+            return "0";
+        }
+    }
+}
