3a4360217d6e49305540c0e59e8584f371791012
Daniel Russom <daniel.russom00@gmail.com>
2022-08-31 22:17:10 +0100

    Bank : Update printStatement refactor

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bank/AccountServiceShould.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bank/AccountServiceShould.cs
@@ -1,9 +1,6 @@
-﻿using Castle.Core.Logging;
-using Katalyst_TDD_Starter.Bank;
+﻿using Katalyst_TDD_Starter.Bank;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
-using System.Collections.Generic;
 
 namespace Katalyst_TDD_Starter.Test.Bank
 {
@@ -11,16 +8,14 @@ namespace Katalyst_TDD_Starter.Test.Bank
     public class AccountServiceShould
     {
         private Mock<IStatementPrinter> StatementPrinter;
-        private Mock<ITimeGetter> TimeGetter;
         private Mock<IStatementLog> StatementLog;
         private AccountService UnderTest;
 
         public AccountServiceShould()
         {
             StatementPrinter = new Mock<IStatementPrinter>();
-            TimeGetter = new Mock<ITimeGetter>();
             StatementLog = new Mock<IStatementLog>();
-            UnderTest = new AccountService(StatementPrinter.Object, TimeGetter.Object, StatementLog.Object);
+            UnderTest = new AccountService(StatementPrinter.Object, StatementLog.Object);
         }
 
         [TestMethod]
@@ -46,20 +41,5 @@ namespace Katalyst_TDD_Starter.Test.Bank
 
             StatementPrinter.Verify(x => x.PrintStatement(It.IsAny<IStatementLog>()), Times.Exactly(1));
         }
-
-        //[TestMethod]
-        //public void Print_populated_statement_log()
-        //{
-        //    List<StatementEntry> printedStatements = null;
-        //    StatementPrinter.Setup(x => x.PrintStatement(It.IsAny<List<StatementEntry>>())).Callback<List<StatementEntry>>(r => printedStatements = r);
-
-        //    UnderTest.Deposit(25);
-        //    UnderTest.Deposit(43);
-        //    UnderTest.PrintStatement();
-
-        //    Assert.AreEqual(2, printedStatements.Count);
-        //    Assert.AreEqual(25, printedStatements[0].Amount);
-        //    Assert.AreEqual(43, printedStatements[1].Amount);
-        //}
     }
 }
--- Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bank/AccountService.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter/Bank/AccountService.cs
@@ -3,31 +3,27 @@
     public class AccountService : IAccountService
     {
         private readonly IStatementPrinter statementPrinter;
-        private readonly ITimeGetter timeGetter;
         private readonly IStatementLog statementLog;
 
-        public AccountService(IStatementPrinter statementPrinter, ITimeGetter timeGetter, IStatementLog statementLog)
+        public AccountService(IStatementPrinter statementPrinter, IStatementLog statementLog)
         {
             this.statementPrinter = statementPrinter;
-            this.timeGetter = timeGetter;
             this.statementLog = statementLog;
         }
-
-        public List<StatementEntry> StatementLog { get; set; } = new();
 
         public void Deposit(int amount)
         {
             statementLog.AddEntry(amount);
         }
 
-        public void PrintStatement()
-        {
-            statementPrinter.PrintStatement(statementLog);
-        }
-
         public void Withdraw(int amount)
         {
             statementLog.AddEntry(-amount);
+        }
+
+        public void PrintStatement()
+        {
+            statementPrinter.PrintStatement(statementLog);
         }
     }
 }
