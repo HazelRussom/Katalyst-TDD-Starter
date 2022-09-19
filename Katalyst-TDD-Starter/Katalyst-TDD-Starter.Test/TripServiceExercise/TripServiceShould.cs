d7de7b3c5b5ef84840b550cd8931cea45e84835f
Daniel Russom <daniel.russom00@gmail.com>
2022-09-19 17:48:12 +0100

    Start testing TripService

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/TripServiceExercise/TripServiceShould.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+
+namespace Katalyst_TDD_Starter.Test.TripServiceExercise
+{
+    [TestClass]
+    public class TripServiceShould
+    {
+        [TestMethod]
+        [Ignore]
+        public void Error_when_user_is_not_logged_in()
+        {
+            var UnderTest = new TripService();
+            TripServiceKata.User.User input = null;
+
+            Assert.ThrowsException<UserNotLoggedInException>(() => UnderTest.GetTripsByUser(input));
+        }
+    }
+}
