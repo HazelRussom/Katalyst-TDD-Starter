685909f1ffdc290cf0a351ac4fd72852b16be3a2
Daniel Russom <daniel.russom00@gmail.com>
2022-12-16 17:33:42 +0000

    Bags acceptance test rough outline

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/BagOrganizerFeatureShould.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.Bags
+{
+    [TestClass]
+    public class BagOrganizerFeatureShould
+    {
+        [TestMethod]
+        public void Organise_bags()
+        {
+
+            // Arrange :
+            // Set up bags with various categories
+            // Populate bags with items of various categories 
+
+
+            // Act:
+            // Organise bags
+
+
+            // Assert:
+            // Bags contain correct items in correct order
+            
+        }
+    }
+}
