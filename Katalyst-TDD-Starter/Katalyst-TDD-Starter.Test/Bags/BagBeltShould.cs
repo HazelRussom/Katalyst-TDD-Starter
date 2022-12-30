f0e0beadbcd4a16035d18f3704eb0287c2834ae6
Daniel Russom <daniel.russom00@gmail.com>
2022-12-30 17:02:10 +0000

    Tidy BagBelt tests

--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/BagBeltShould.cs
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/BagBeltShould.cs
@@ -11,6 +11,14 @@ namespace Katalyst_TDD_Starter.Test.Bags
     {
         private readonly Item _testItem = new("TestItem", ItemCategory.Cloth);
         private readonly BagBelt _underTest = new();
+        private readonly Mock<IBag> _bagWithSpace = new();
+        private readonly Mock<IBag> _bagWithoutSpace = new();
+
+        public BagBeltShould()
+        {
+            _bagWithSpace.Setup(x => x.HasSpace()).Returns(true);
+            _bagWithoutSpace.Setup(x => x.HasSpace()).Returns(false);
+        }
 
         [TestMethod]
         public void Start_with_no_bags()
@@ -21,60 +29,48 @@ namespace Katalyst_TDD_Starter.Test.Bags
         [TestMethod]
         public void Add_bags()
         {
-            var firstBagToAdd = new Bag(4);
-            var secondBagToAdd = new Bag(ItemCategory.Metal, 4);
-
-            _underTest.AddBag(firstBagToAdd);
-            _underTest.AddBag(secondBagToAdd);
+            _underTest.AddBag(_bagWithSpace.Object);
+            _underTest.AddBag(_bagWithoutSpace.Object);
 
             var bags = _underTest.GetBags();
-            Assert.IsTrue(bags.Contains(firstBagToAdd));
-            Assert.IsTrue(bags.Contains(secondBagToAdd));
+            Assert.IsTrue(bags.Contains(_bagWithSpace.Object));
+            Assert.IsTrue(bags.Contains(_bagWithoutSpace.Object));
             Assert.AreEqual(2, bags.Count);
         }
 
         [TestMethod]
         public void Add_item_to_empty_bag()
         {
-            var bag = new Mock<IBag>();
-            bag.Setup(x => x.HasSpace()).Returns(true);
-            _underTest.AddBag(bag.Object);
+            _underTest.AddBag(_bagWithSpace.Object);
 
             _underTest.AddItem(_testItem);
 
-            bag.Verify(x => x.AddItem(_testItem), Times.Once);
+            _bagWithSpace.Verify(x => x.AddItem(_testItem), Times.Once);
         }
 
         [TestMethod]
-        public void Add_item_when_first_bag_is_full()
+        public void Add_item_to_second_bag_when_first_bag_is_full()
         {
-            var fullBag = new Mock<IBag>();
-            fullBag.Setup(x => x.HasSpace()).Returns(false);
-            _underTest.AddBag(fullBag.Object);
-            var emptyBag = new Mock<IBag>();
-            emptyBag.Setup(x => x.HasSpace()).Returns(true);
-            _underTest.AddBag(emptyBag.Object);
+            _underTest.AddBag(_bagWithoutSpace.Object);
+            _underTest.AddBag(_bagWithSpace.Object);
             
             _underTest.AddItem(_testItem);
 
-            fullBag.Verify(x => x.AddItem(_testItem), Times.Never);
-            emptyBag.Verify(x => x.AddItem(_testItem), Times.Once);
+            _bagWithoutSpace.Verify(x => x.AddItem(_testItem), Times.Never);
+            _bagWithSpace.Verify(x => x.AddItem(_testItem), Times.Once);
         }
 
         [TestMethod]
         public void Not_add_item_when_all_bags_are_full()
         {
-            var fullBag = new Mock<IBag>();
-            fullBag.Setup(x => x.HasSpace()).Returns(false);
-            _underTest.AddBag(fullBag.Object);
+            _underTest.AddBag(_bagWithoutSpace.Object);
 
             var exception = Assert.ThrowsException<BagException>(() => _underTest.AddItem(_testItem));
-
-            fullBag.Verify(x => x.AddItem(_testItem), Times.Never);
+            //TODO Can we take the exception without asserting?
+            _bagWithoutSpace.Verify(x => x.AddItem(_testItem), Times.Never);
             Assert.AreEqual("All bags are full, no more items can be added!", exception.Message);
         }
 
-        // Bag belt should tell each bag to organise itself (alphabetisation)
         [TestMethod]
         public void Tell_bags_to_organise()
         {
@@ -85,7 +81,7 @@ namespace Katalyst_TDD_Starter.Test.Bags
 
             bag.Verify(x => x.Organise(), Times.Once);
         }
-
+        // Test for multiple bags
         // Bag belt should... do something to move items into correct bag
 
     }
--- Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/Bags Brainstorm.md
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Bags/Bags Brainstorm.md
@@ -30,3 +30,36 @@ Display items from a bag
 Organise bags
 
 
+
+
+
+### Dumping from Organizer class
+  //[TestMethod]
+        //public void Alphabetize_bag_contents()
+        //{
+
+        //    var underTest = new BagBeltOrganizer();
+
+        //    var unsortedBagBelt = new BagBelt();
+        //    var clothBag = new Bag(ItemCategory.Cloth, 4);
+        //    unsortedBagBelt.AddBag(clothBag);
+        //    var firstAlphabeticalItem = new Item("TestItem", ItemCategory.Cloth);
+        //    var secondAlphabeticalItem = new Item("XTestItem", ItemCategory.Cloth);
+
+        //    clothBag.AddItem(secondAlphabeticalItem);
+        //    clothBag.AddItem(firstAlphabeticalItem);
+
+        //    var bagBeltResult = underTest.Organize(unsortedBagBelt);
+        //    var bagResult = bagBeltResult.GetBags()[0];
+
+        //    Assert.AreEqual(firstAlphabeticalItem, bagResult.Items[0]);
+        //    Assert.AreEqual(firstAlphabeticalItem, bagResult.Items[1]);
+        //}
+
+        // Organisation 
+        // Move single item to category bag
+        // Move multiple items to different category bags
+        // Leave item in first bag when no category bag exists
+        // Leave item in first bag when only full category bags exist
+        // Alphabetize items in bags
+        // Test for when all bags are full and unordered
