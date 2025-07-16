41aa7421be20b4757a67cc347f17797e16d7c3f2
Hazel Russom <hazel.russom@gmail.com>
2025-07-16 14:54:23 +0100

    Add Arithmetic acceptance test

---
+++ Katalyst-TDD-Starter/Katalyst-TDD-Starter.Test/Arithmetics/ArithmeticFeatureShould.cs
@@ -0,0 +1,21 @@
+﻿using Katalyst_TDD_Starter.Arithmetics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katalyst_TDD_Starter.Test.Arithmetics;
+
+[TestClass]
+public class ArithmeticFeatureShould
+{
+    [TestMethod]
+    [DataRow("( 1 + ( ( 2 + 3 ) * (4 * 5) ) )", 101)]
+    [DataRow("( 5 * ( 4 * ( 3 * ( 2 * ( 1 * 9 ) / 8 - 7 ) + 6 ) ) )", 3)]
+    [DataRow("((()()))", 0)]
+    public void Calculate_example_answers(string input, int expectedResult)
+    {
+        var arithmeticCalculator = new ArithmeticCalculator();
+
+        var actualResult = arithmeticCalculator.Calculate(input);
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
